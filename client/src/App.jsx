cad758e7969f3518ab95ced279b86f0184412d79
Vishesh <ranavisheshsingh242@gmail.com>
2023-10-01 19:31:12 +0530

    Components and pages created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
-
-export default function App() {
+import React from "react";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+const App = () => {
   return (
-    <div className='text-red-500'>App</div>
+  <BrowserRouter>
+  <Header/>
+  <Routes>
+  <Route path="/" element={<Home/>}/>
+  <Route path="/sign-in" element={<SignIn/>}/>
+  <Route path="/sign-up" element={<SignUp/>}/>
+  <Route path="/about" element={<About/>}/>
+  <Route path="/profile" element={<Profile/>}/>
+  </Routes>
+  </BrowserRouter>
   )
-}
+};
+
+export default App;
