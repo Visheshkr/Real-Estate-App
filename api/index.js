c3b6434abda4cbbfdc92b3181d873e3ffc3b7abc
Vishesh <ranavisheshsingh242@gmail.com>
2023-10-02 20:15:23 +0530

    complete sig up page functionality

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.use((err,req,res,next)=>{
     const statusCode=err.statusCode||500;//internal server error
     const message=err.message || 'Internal Server Error';
     return res.status(statusCode).json({
-        successL:false,
+        success:false,
         statusCode,
         message,
     });
