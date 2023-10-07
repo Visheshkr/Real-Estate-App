3123c009a986ee97600a186392a8573e19ce5c06
Vishesh <ranavisheshsingh242@gmail.com>
2023-10-07 20:14:16 +0530

    create search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -57,4 +57,54 @@ export const updateListing =async(req,res,next)=>{
     } catch (error) {
       next(error);
     }
-  }
+  };
+
+  export const getListings =async(req,res,next)=>{
+    try {
+      const limit = parseInt(req.query.limit) || 9;//for limiting the items on a page max to 9
+      const startIndex = parseInt(req.query.startIndex) || 0;
+      let offer =req.query.offer;
+
+      if(offer === undefined || offer ===false){
+        offer ={$in : [false,true]};//by default showings all offer and nonoffers places to the user and searching in database
+      }
+      let furnished =req.query.furnished;
+
+      if(furnished === undefined || furnished ===false){
+        furnished ={$in : [false,true]};
+      }
+
+      let parking =req.query.parking;
+
+      if(parking === undefined || parking ===false){
+        parking ={$in : [false,true]};
+      }
+
+      let type =req.query.type;
+
+      if(type===undefined || type==='all'){
+        type={$in :['sale','rent']};
+      }
+
+      const searchTerm = req.query.searchTerm || '';
+
+      const sort =req.query.sort ||'createdAt';
+
+      const order =req.query.order ||'desc';
+      //regex is builtin searching functionality of mongodb and it searches for even the part of the query on only the whole word and the i is used to ignore the case of the searched results
+      const listings =await Listing.find({
+        name:{$regex :searchTerm , $options :'i'},
+        offer,
+        furnished,
+        parking,
+        type,
+      }).sort(
+        {[sort]:order}
+      ).limit(limit).skip(startIndex);
+
+      return res.status(200).json(listings);
+
+    } catch (error) {
+      next(error);
+    }
+  };
